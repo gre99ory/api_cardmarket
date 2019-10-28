97951167a50a1325d79b1009141e9c29088d8a32
Dmitriy Kovalev Home <kodimweb@gmail.com>
2019-10-28 23:48:33 +0300

    adeed settings page

--- index.php
+++ index.php
@@ -18,27 +18,37 @@
     register_activation_hook( __FILE__, 'mkm_api_create_table' );
     add_action( 'admin_menu', 'mkm_api_admin_menu' );
 
-function mkm_api_create_table() {
-    global $wpdb;
-    $query = "CREATE TABLE IF NOT EXISTS `mkm_api_settings` (
-        `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-        `api_key` varchar(100) NOT NULL,
-        `api_set` varchar(255) NOT NULL,
-        PRIMARY KEY (`id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+    function mkm_api_create_table() {
+        global $wpdb;
+        $query = "CREATE TABLE IF NOT EXISTS `mkm_api_settings` (
+            `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+            `api_key` varchar(100) NOT NULL,
+            `api_set` varchar(255) NOT NULL,
+            PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 
-    $wpdb->query($query);
-}
+        $wpdb->query($query);
+    }
 
-function mkm_api_admin_menu() {
-    add_options_page( 'MKM API', 'MKM API', 'manage_options', 'mkm-api-plugin', 'mkm_api_page' );
-}
+    function mkm_api_admin_menu() {
+        add_menu_page( 'MKM API', 'MKM API', 'manage_options', 'mkm-api-options', 'mkm_api_options', 'dashicons-groups' );
 
-function mkm_api_page() {
-    ?>
+        add_submenu_page( 'mkm-api-options', 'MKM API DATA', 'API data', 'manage_options', 'mkm-api-subpage', 'mkm_api_data' );
+    }
 
-        <div class="mkm-api-wrap">
-            <h2><?php _e( 'MKM API Setings', 'mkm-api' ); ?></h2>
-        </div>
-    <?php
-}
+    function mkm_api_options( ) {
+        ?>
+
+            <div class="wrap">
+                <h2><?php _e( 'MKM API Setings', 'mkm-api' ); ?></h2>
+                <form action="options.php" method="post">
+                
+                <?php submit_button( __( 'Add API', 'mkm-api' ) ); ?>
+                </form>
+            </div>
+        <?php
+    }
+
+    function mkm_api_data() {
+
+    }
