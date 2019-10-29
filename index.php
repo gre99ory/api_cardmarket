f19596ab8d9ef9168fc70da11a4e4d54b0b43ba8
Dmitriy Kovalev <kodimweb@gmail.com>
2019-10-29 14:05:42 +0300

    added settings page

--- index.php
+++ index.php
@@ -41,34 +41,73 @@
         //option_group, option_name, sanitize_callback
         register_setting( 'mkm_api_group_options', 'mkm_api_options', 'mkm_api_sanitize' );
 
-        add_settings_section( 'mkm_api_options_section_id', '', '', 'mkm-api-options' );
-
-        add_settings_field( 'mkm_api_setting_key_id', __( 'Key', 'mkm-api' ), 'mkm_api_setting_key_cd', 'mkm-api-options', 'mkm_api_options_section_id', array( 'label_for' => 'mkm_api_setting_key_id' ) );
     }
 
-    function mkm_api_sanitize() {
+    function mkm_api_sanitize( $option ) {
 
+        $add_array             = array();
+        $select                = array( 'min', 'hours', 'days' );
+        $arr                   = ( is_array( get_option( 'mkm_api_options' ) ) && count( get_option( 'mkm_api_options' ) ) > 0 ) ? get_option( 'mkm_api_options' ) : array();
+
+        if ( $option['key'] == '' ) return $arr;
+
+        $add_array['key']      = $option['key'];
+        $add_array['interval'] = (int)$option['key'] == 0 ? 1 : $option['key'];
+        if ( in_array( $option['time'], $select ) ) {
+            $add_array['time'] = $option['time'];
+        } else {
+            $add_array['time'] = 'min';
+        }
+
+        $arr[] = $add_array;
+
+        return $arr;
     }
 
-    function mkm_api_setting_key_cd() {
-        $options = get_option( 'mkm_api_options' );
-
-        ?>
-        <p>
-            <input type="text" value="" class="regular-text" name="mkm_api_options" id="mkm_api_setting_key_id" >
-        </p>
-
-        <?php
-    }
 
     function mkm_api_options( ) {
+        $option = get_option( 'mkm_api_options' );
+        $time   = array( 'min' => __( 'minutes', 'mkm-api' ), 'hours' => __( 'hours', 'mkm-api' ), 'days' => __( 'days', 'mkm-api' ) );
         ?>
 
             <div class="wrap">
                 <h2><?php _e( 'MKM API Settings', 'mkm-api' ); ?></h2>
                 <form action="options.php" method="post">
-                <?php settings_fields( 'mkm_api_group_options' ); ?>
-                <?php do_settings_sections( 'mkm-api-options' ); ?>
+                    <?php settings_fields( 'mkm_api_group_options' ); ?>
+                    <table class="form-table">
+                    <?php if ( is_array( $option ) && count( $option ) > 0 ) {  ?>
+                        <tr>
+                            <th></th>
+                            <td>
+                                <table>
+                                    <?php foreach( $option as $item ){ ?>
+                                    <?php $interval = $item['interval'] . ' ' . $time[$item['time']]; ?>
+                                        <tr>
+                                            <td style="width: 50%;"><span style="font-style: italic;"><?php echo $item['key']; ?></span></td>
+                                            <td><span>(<?php _e( 'Interval', 'mkm-api' ); ?> : <?php echo $interval; ?>)</span></td>
+                                            <td style="width: 10%;"><a href="" style="color: red;"><?php _e( 'Delete', 'mkm-api' ); ?></a></td>
+                                        </tr>
+                                    <?php } ?>
+                                </table>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                        <tr>
+                            <th><label for="mkm_api_setting_key_id"><?php _e( 'Key', 'mkm-api' ); ?></label></th>
+                            <td>
+                                <input type="text" value="" class="regular-text" name="mkm_api_options[key]" id="mkm_api_setting_key_id" >
+                                <label for="mkm_api_setting_interval_id"><?php _e( 'Interval', 'mkm-api' ); ?></label>
+                                <input type="number" value="" class="small-text" name="mkm_api_options[interval]" id="mkm_api_setting_interval_id" >
+                                <label for="mkm_api_setting_time_id"><?php _e( 'Time', 'mkm-api' ); ?></label>
+                                <select name="mkm_api_options[time]" id="mkm_api_setting_time_id">
+                                <?php foreach ( $time as $time_key => $time_val ) { ?>
+                                    <option value="<?php echo $time_key; ?>"><?php echo $time_val; ?></option>
+                                <?php } ?>
+                                </select>
+                            </td>
+                        </tr>
+                    </table>
+
                 <?php submit_button( __( 'Add API', 'mkm-api' ) ); ?>
                 </form>
             </div>
